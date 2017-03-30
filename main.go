7a449d3cbb7203fb6cc9772bd4cede97942b17ff
vuurball <oneindigedroom@gmail.com>
2017-03-30 12:15:35 +0300

    process post + redis con
    finished basic draft of processing vees post, 
    processing a post for skills 
    and added example of how to connect to redis

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"net/http"
 	"strings"
 	 "strconv"
 	 "io/ioutil"
+	 "github.com/go-redis/redis"
 )
 
 
@@ -16,32 +18,61 @@ func scrapeSeev(){
 	nextPage := true
 	page := 1
 	url := firstUrl + strconv.Itoa(page)
+	pagePostsHtml := []string{}
 
-	for nextPage == true{
-		fmt.Println("SCRAPING:" ,url)
+	for nextPage == true {
+		//fmt.Println("SCRAPING:" ,url)
 		resp, _ := http.Get(url)
 		bytes, _ := ioutil.ReadAll(resp.Body)
-		pageHtml :=string(bytes)
+		pageFullHtml := string(bytes)
+
+		if strings.Index(pageFullHtml, "<div class=\"job row") > -1{
+            nextPage = true
+            page++
+            url = firstUrl + strconv.Itoa(page)
+            //fmt.Println("next page found")
+        } else{
+        	//fmt.Println("next page not found")
+            nextPage = false // Setting nextPage to FALSE if there's no 'Next' link
+        }
+
+        pagePostsHtml = strings.Split(pageFullHtml, "class=\"job row\"")
+        pagePostsHtml = append(pagePostsHtml[1:len(pagePostsHtml)]) //remove the first
+        //fmt.Println(len(pagePostsHtml))
+
+        for _, postContentHtml := range pagePostsHtml {
+        	if postKey := ""; postContentHtml != "" {
+        		postKey = scrapeBetween(postContentHtml, "data-id=\"", "\">")
+        		fmt.Println(postKey)
+        		//if Redis::sismember(key, postKey) === 0 {
+        			processPost(postContentHtml)
+        		//}
+        	}
+        	os.Exit(3)
+        }
 		//fmt.Println("HTML:\n\n", string(bytes))
 		nextPage = false
 	}	
 
-	fmt.Println("loaded", url, key)
-	skills := [7]string{"php", "mysql", "linux", "apache", "css3", "html5", "javascript"}
-	//fmt.Println(skills)
+	fmt.Println("finished running", url, key)
 
-	post := "<div class=\"jobFields\"><spancss3 php mysql Linux class=\"sendCV\"><a action=\"https://www.drushim.co.il/sendcv.a"
+}
+
+func getAllSkillNames() []string {
+	return []string{"php", "mysql","api","rest", "json", "git","linux", "apache", "css3", "html5", "javascript"}
+}
+
+func processPost(post string){
 	//fmt.Println(post)
-
+	fmt.Println("processed new post")
+	// Redis::incr('postsCounter');
+	skills := getAllSkillNames();
 	var foundSkills []string
-	fmt.Println(foundSkills)
-	
-	//fmt.Println(strings.ToLower(post))
 	post = strings.ToLower(post)
 
 	for _, skill := range skills {
 		if strings.Index(post, skill) > -1 {
-			//fmt.Println("skill", skill, "found")
+			
 			foundSkills = append(foundSkills, skill)
 		}
 	}
@@ -57,7 +88,51 @@ func scrapeSeev(){
 	}
 }
 
+func scrapeBetween(data string, start string, end string) string{
+ 	startIndex := strings.Index(data, start)
+    if startIndex == -1 {
+        return ""
+    }
+    startIndex += len(start)
+    endIndex := strings.Index(data, end)
+    if endIndex == -1 {
+        return ""
+    }
+    return data[startIndex:endIndex]
+}
+
 func main(){
-	scrapeSeev()
+    scrapeSeev()
 	http.ListenAndServe(":8080", nil)    
+}
+
+func redisConnectionDraft(){
+	client := redis.NewClient(&redis.Options{
+		Addr: "10.0.8.201:6379",
+		Password: "", // no password set
+		DB: 0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	err = client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exists")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
 }
