b77b08026a97ddd46c8c06fd9eec70491c833158
vuurball <oneindigedroom@gmail.com>
2017-04-02 14:33:27 +0300

    added conccurency
    scrape all sites at the same time

--- main.go
+++ main.go
@@ -2,52 +2,35 @@ package main
 
 import (
  	"scraper"
- 	// "fmt"
- 	//"github.com/go-redis/redis"
+ 	 . "fmt"
+ 	 "sync"
+ 	 "time"
 )
 
 
+var wg sync.WaitGroup
+
 func main(){
-
-	dataSources :=[]string{"seev", "drushim", "nisha"}
-
-	for _, dataSource := range dataSources{	
-		s, ok:= scraper.GetScraperInstance("Scraper"+dataSource)
-		if ok{
-	    	s.Scrape()	
-		}
-	}
 	
+	start := time.Now()
+	dataSources := []string{"seev", "drushim", "nisha"}
+
+	wg.Add(1)
+	go func() {
+		for _, dataSource := range dataSources{	
+			s, ok:= scraper.GetScraperInstance("Scraper"+dataSource)
+			if ok{
+		    	s.Scrape()	
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	elapsed := time.Since(start)
+    Println("time:",elapsed)
 	//http.ListenAndServe(":8080", nil)    
 }
 
-// func redisConnectionDraft(){
-// 	client := redis.NewClient(&redis.Options{
-// 		Addr: "10.0.8.201:6379",
-// 		Password: "", // no password set
-// 		DB: 0,  // use default DB
-// 	})
-
-// 	pong, err := client.Ping().Result()
-// 	fmt.Println(pong, err)
-
-// 	err = client.Set("key", "value", 0).Err()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	val, err := client.Get("key").Result()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Println("key", val)
-
-// 	val2, err := client.Get("key2").Result()
-// 	if err == redis.Nil {
-// 		fmt.Println("key2 does not exists")
-// 	} else if err != nil {
-// 		panic(err)
-// 	} else {
-// 		fmt.Println("key2", val2)
-// 	}
-// }
+ 
